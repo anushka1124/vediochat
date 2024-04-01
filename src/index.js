6d32a961383db8b198306a2f5e0cdb30a4abadd1
anushka1124 <anushkasrivastava1124@gmail.com>
2024-04-01 21:42:40 +0530

    utilities

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Node connection failed : ", error);
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+}).catch((err) => {
+    console.log("MONGODB CONNECTION FAILED !!! ", err)
+});
 
 
 
