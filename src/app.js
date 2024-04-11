7e7e37bfcf7dccb16835da7bef75636827a5fcb2
anushka1124 <anushkasrivastava1124@gmail.com>
2024-04-11 17:07:45 +0530

    file add

--- src/app.js
+++ src/app.js
@@ -20,13 +20,5 @@ import userRouter from './routes/user.routes.js'
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
-// app.use("/api/v1/healthcheck",)
-// app.use("/api/v1/tweets",)
-// app.use("/api/v1/subscriptions",)
-// app.use("/api/v1/vedios",)
-// app.use("/api/v1/comments",)
-// app.use("/api/v1/likes",)
-// app.use("/api/v1/playlist",)
-// app.use("/api/v1/dashboard",)
 
 export { app };
---
+++ src/routes/video.routes.js
@@ -0,0 +1,27 @@
+// import { Router } from "express";
+
+// import {
+
+// } from "../controllers/vedio.controller.js"
+// import { verifyJWT } from "../middlewares/auth.middleware.js";
+// import { upload } from "../middlewares/multer.middleware.js";
+
+// const router = Router();
+// router.route(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+// router
+//     .route("/")
+//     .get(getAllVedios)
+//     .post(
+//         upload.fields([
+//             {
+//                 name: "videoFile",
+//                 maxCount: 1,
+//             },
+//             {
+//                 name: "thumbnail",
+//                 maxCount: 1,
+//             }
+//         ]),
+//         publishAVideo
+//     )
